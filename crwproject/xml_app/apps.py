0d488ecf23c310cf3e4604d0ce83978729fadff8
Enes <enestokol@gmail.com>
2021-09-04 19:25:56 +0300

    Add django signals settings

--- crwproject/xml_app/apps.py
+++ crwproject/xml_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class XmlAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'crwproject.xml_app'
+
+    def ready(self):
+        import crwproject.xml_app.signals
