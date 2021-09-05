1f940fc9710a5e93c1b668d2ee7d23a3877e2b98
Enes <enestokol@gmail.com>
2021-09-05 19:02:04 +0300

    Add delete signals so that when the file is deleted from the database it is also deleted from the directory.

--- crwproject/xml_app/signals.py
+++ crwproject/xml_app/signals.py
@@ -1,11 +1,15 @@
-from django.db.models.signals import pre_save
+import os
+
+from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 from crwproject.api.tasks import *
 
 
 @receiver(pre_save, sender=File)
 def send_email_on_create_from_file(sender, instance, **kwargs):
+    print("8")
     if instance.status == '2':
+        print("10")
         _subject = 'About {0}'.format(instance.name)
         _message = '{0} was {1} Due to {2}'.format(
             instance.name,
@@ -13,3 +17,29 @@ def send_email_on_create_from_file(sender, instance, **kwargs):
             instance.status_note)
         # _to = instance.created_by.email
         # sendEmail.delay(_subject, _message, _to)
+
+
+@receiver(post_delete, sender=File)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+
+
+@receiver(pre_save, sender=File)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = File.objects.get(pk=instance.pk).file
+    except File.DoesNotExist:
+        return False
+
+    new_file = instance.file
+    if not old_file == new_file:
+        try:
+            if os.path.isfile(old_file.path):
+                os.remove(old_file.path)
+        except Exception:
+            return False
