e21494fa153c911cf6c9f19b320ac88e303c0f4b
Enes <enestokol@gmail.com>
2021-09-05 19:01:09 +0300

    Remove unused fields

--- crwproject/auth_app/models.py
+++ crwproject/auth_app/models.py
@@ -36,8 +36,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    is_referee = models.BooleanField(default=False)
-    is_observer = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
     address = models.TextField(null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
